e9f0aa328e72b1b389a10e977a93f599f86a047a
yangyile <example@gogit.com>
2024-05-23 19:35:59 +0700

    增加combine函数把要select的列合起来

--- sx.go
+++ sx.go
@@ -1,5 +1,7 @@
 package gormcnm
 
+import "strings"
+
 type SxType struct {
 	*queryArgsTuple
 }
@@ -15,4 +17,17 @@ func NewSx(qs string, args ...interface{}) *SxType {
 
 func Sx(qs string, args ...interface{}) *SxType {
 	return NewSx(qs, args...)
+}
+
+func (sx *SxType) Combine(cs ...*SxType) *SxType {
+	var qsVs []string
+	qsVs = append(qsVs, sx.Qs())
+	var args []any
+	args = append(args, sx.Args()...)
+	for _, c := range cs {
+		qsVs = append(qsVs, c.Qs())
+		args = append(args, c.Args()...)
+	}
+	var stmt = strings.Join(qsVs, ", ") //得到的就是gorm db.Select() 的要选中的列信息，因此使用逗号分隔
+	return NewSx(stmt, args...)         //得到的就是 gorm db.Select() 的选中信息和附带的参数信息，比如 COUNT(CASE WHEN condition THEN 1 END) 里 condition 的参数信息
 }
